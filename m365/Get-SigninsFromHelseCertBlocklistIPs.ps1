ea4c923bc493a6d9cfbb58891a0b7db54dbe8645
kjellchr <kjellchr@abuse.ch>
2024-03-11 10:44:27 +0100

    Lagt til $ foran variabelnavn

--- m365/Get-SigninsFromHelseCertBlocklistIPs.ps1
+++ m365/Get-SigninsFromHelseCertBlocklistIPs.ps1
@@ -72,17 +72,17 @@ Get-Content $configfil | ForEach-Object {
   if($val -ne $null) {
       $key = $key.Trim()
       $val = $val.Trim()
-      if ($key -eq 'TenantId') { $TenantId=$val } 
-      elseif ($key -eq 'AppId') { $AppId=$val } 
-      elseif ($key -eq 'CertificateThumbprint') { $CertificateThumbprint=$val } 
-      elseif ($key -eq 'NBPuser') { $NBPuser=$val }
-      elseif ($key -eq 'NBPpass') { $NBPpass=$val } 
-      elseif ($key -eq 'smtpserver') { $smtpserver=$val }
-      elseif ($key -eq 'smtpto') { $smtpto=$val }
-      elseif ($key -eq 'smtpfrom') { $smtpfrom=$val }
-      elseif ($key -eq 'NamedLocations') { $NamedLocations=$val }
-      elseif ($key -eq 'Signinslog') { $Signinslog=$val }
-      elseif ($key -eq 'blocklistdomain') { $blocklistdomain=$val }
+      if ($key -eq '$TenantId') { $TenantId=$val } 
+      elseif ($key -eq '$AppId') { $AppId=$val } 
+      elseif ($key -eq '$CertificateThumbprint') { $CertificateThumbprint=$val } 
+      elseif ($key -eq '$NBPuser') { $NBPuser=$val }
+      elseif ($key -eq '$NBPpass') { $NBPpass=$val } 
+      elseif ($key -eq '$smtpserver') { $smtpserver=$val }
+      elseif ($key -eq '$smtpto') { $smtpto=$val }
+      elseif ($key -eq '$smtpfrom') { $smtpfrom=$val }
+      elseif ($key -eq '$NamedLocations') { $NamedLocations=$val }
+      elseif ($key -eq '$Signinslog') { $Signinslog=$val }
+      elseif ($key -eq '$blocklistdomain') { $blocklistdomain=$val }
 }
 
 # Sjekker om variabler er endret før kjøring
